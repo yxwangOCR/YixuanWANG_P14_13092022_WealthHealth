2bbeac9a3044e1b26c7a6debf935b0093082c870
Hello Git <wang.yixuan@live.fr>
2022-09-14 01:57:50 -0400

    material-table

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import MaterialTable from "material-table";
+
+
+function Table() {
+  return (
+    <div style={{ maxWidth: "100%" }}>
+      <MaterialTable
+        columns={[
+          { title: "Adı", field: "name" },
+          { title: "Soyadı", field: "surname" },
+          { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+          {
+            title: "Doğum Yeri",
+            field: "birthCity",
+            lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+          },
+        ]}
+        data={[
+          {
+            name: "Mehmet",
+            surname: "Baran",
+            birthYear: 1987,
+            birthCity: 63,
+          },
+        ]}
+        title="Demo Title"
+      />
+    </div>
+  );
+}
+
+export default Table;
--- src/pages/Employee/Employee.jsx
+++ src/pages/Employee/Employee.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Table from "../../components/Table/Table";
 
 function Employee() {
   return (
     <div>
       <h1>Current Employees</h1>
       <Link to="/" className="view-home-link">
+        <Table></Table>
         Home
       </Link>
     </div>
